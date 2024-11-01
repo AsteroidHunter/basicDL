6802a57cbc0edd9897d35ce603af0f1f82430201
Eric Duong <eduong@arizona.edu>
2024-10-31 19:20:49 -0700

    install script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # basicDL
+
+Create conda environment, activate it, and then run
+```bash
+bash install.sh
+```
---
+++ install.sh
@@ -0,0 +1,1 @@
+conda install numpy scipy scikit-learn pandas matplotlib seaborn notebook pymc pytorch torchvision torchaudio cpuonly -c pytorch
